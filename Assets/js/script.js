9d420a76973b26119d50819215924cff5282618a
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-04-07 17:51:14 +1000

    saveBtn func added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,14 @@
-var currentDay = document.getElementById("currentDay");
-var today = moment();
+// var currentDay = document.getElementById("currentDay");
+var currentDay = $("#currentDay");
+var saveBtn = $(".saveBtn");
+var description = $(".description");
+var today = moment();// This line stores moment Datetime data in today variable.
 
-
+// These lines display formated datetime on the page.
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+// 
+saveBtn.on("click", function () {
+    $("description").val("");
+    // console.log("works");
+})

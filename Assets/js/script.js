50ea32885881dca8fdde5fd6ec7c36249d5f86d4
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-04-08 23:24:19 +1000

    if statemnet added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,7 +7,7 @@ var today = moment();// This line stores moment Datetime data in today variable.
 // These lines display formated datetime on the page.
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
-var currentTime = new Date().getHours();
+var currentTime = new Date().getHours(); // This method give the hours as number and store in currentTime.
 
 // These store user inputs in the local storage.
 $(document).ready(function(){
@@ -24,8 +24,14 @@ $(document).ready(function(){
         console.log("element", this); // This line gets tim block element
     
         console.log($(this).attr("id").split("-")[1]); // This line split the hour from #id.
-        
-        
+
+        if (currentTime > this) {
+            time-block.classList.add("past");
+        }else if (currentTime == this){
+            time-block.classList.add("present");
+        }else {
+            time-block.classList.add("future");
+        }
     });
 
 });
@@ -36,3 +42,4 @@ $(document).ready(function(){
 // time-block.classList.add("past");
 // var currentTime = new Date().getTime();
 // $(selector).attr(attribute)
+// 9 < currentTime 

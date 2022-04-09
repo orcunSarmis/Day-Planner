12e16d991b2f3c4722b432745239590c19d343fa
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-04-09 11:13:59 +1000

    eventlsitner added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,6 +3,7 @@ var currentDay = $("#currentDay");
 var saveBtn = $(".saveBtn");
 var description = $(".description");
 var today = moment();// This line stores moment Datetime data in today variable.
+var body = $("body");
 
 // These lines display formated datetime on the page.
 $("#currentDay").text(today.format("dddd, MMMM Do"));
@@ -14,25 +15,35 @@ $(document).ready(function(){
     $(".saveBtn").click(function(){
        var str = $(this).siblings(".description").val(); //Takew inputs from texteria and buttons which same level under div parent.
        var hour = $(this).parent().attr("id") // and added with id.
-        localStorage.setItem("description", "str"); // Set inputs in local storage.
+        // localStorage.setItem("description", "str"); // Set inputs in local storage.
         localStorage.setItem(hour, str);
 
     });
     
     // These lines will give colors in the fields.
-    $(".time-block").each(function(index) {
-        console.log("element", this); // This line gets tim block element
-    
+    $(".time-block").each(function(index) {    
         var notedHour = parseInt($(this).attr("id").split("-")[1]); // This line split the hour from #id.
-        if (currentTime < notedHour) {
+       
+        if (notedHour < currentTime) {
             $(this).addClass("past");
-        }else if (currentTime == notedHour){
+        }else if (currentTime === notedHour){
             $(this).addClass("present");
         }else {
             $(this).addClass("future");
         }
     });
 
+});
+
+window.addEventListener("load", function(){
+    $(".time-block").each(function(){
+        var dataAttr = $(this).attr("id");
+        // console.log(dataAttr);
+        var userNote = localStorage.getItem(dataAttr);
+
+        var currentElNotes = $(this).children([1]);
+        currentElNotes.val(userNote);
+    })
 });
 
 // var currentTime = new Date().getTime();
